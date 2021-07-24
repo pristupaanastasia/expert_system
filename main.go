be6650e5053dd2cf079bf8736823dc40c685cb81
mriley <pristupa1998@mail.ru>
2021-07-24 17:26:10 +0300

    algorithm REFACTOR

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 )
+
 /*
 commits := map[string]int{
     "rsc": 3711,
@@ -16,9 +17,9 @@ commits := map[string]int{
     "gri": 1908,
     "adg": 912,
 }
- */
+*/
 var (
-	allowedSymbols	string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ=><+|!^?()"
+	allowedSymbols string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ=><+|!^?()"
 )
 
 type point struct {
@@ -35,21 +36,45 @@ func and(a bool, b bool) bool {
 func xor(a bool, b bool) bool {
 	return (a || b) && !(a && b)
 }
-func ifOnlyIf(a bool, b bool) bool {
-	if (a && b) {
-		return true
+func ifOnlyIf(a bool, b bool, res bool) (bool, bool) {
+	if a && b {
+		return true, b
 	}
-	if (!a && !b) {
-		return true
+	if !a && !b {
+		return true, b
 	}
-	return false
+	if res {
+		if b == false {
+			b = true
+		} else {
+			b = false
+		}
+		if a && b {
+			return true, b
+		}
+		if !a && !b {
+			return true, b
+		}
+	}
+
+	return false, false
 }
 
-func implies(a bool, b bool) bool {
-	if a && !b {
-		return false
+func implies(a bool, b bool, res bool) (bool, bool) {
+	if a && !b && !res {
+		return false, false
 	}
-	return true
+	if res {
+		if b == false {
+			b = true
+		} else {
+			b = false
+		}
+		if a && !b && !res {
+			return false, b
+		}
+	}
+	return true, b
 
 }
 func initOper() map[byte]int {
@@ -65,33 +90,62 @@ func initOper() map[byte]int {
 	return oper
 }
 
-func recurs(mass []byte, valueByte map[byte]bool) bool {
+func recurs(mass []byte, valueByte map[byte]int) bool {
 	st := make([]bool, 0)
 
-//	log.Println(string(mass))
-	for _, val := range mass {
-		if  (val >= 'A') && (val <= 'Z') {
-			if _, ok := valueByte[val]; !ok  {
-				valueByte[val] = false
+	res := false
+	log.Println(string(mass))
+	for i, val := range mass {
+		if (val >= 'A') && (val <= 'Z') {
+			if _, ok := valueByte[val]; !ok {
+				return false
 			}
-			st = append(st, valueByte[val])
+			if valueByte[val] == 1 {
+				st = append(st, true)
+			}
+			if valueByte[val] == -1 {
+				st = append(st, false)
+			}
+			if valueByte[val] == 0 {
+				if i < len(mass) && (mass[i+1] == '=' || mass[i+1] == '-') { //а вдруг тут =>a&b  ab&=
+					res = true
+					st = append(st, false)
+				} else {
+					res = true
+					st = append(st, false)
+				}
+			}
 
 		} else {
-			
+
 			if val == '!' && len(st) > 0 {
-
 				st[len(st)-1] = !st[len(st)-1]
-
 			} else {
+				var b bool
 				if len(st) > 1 {
 					var buf bool
 					if val == '-' {
-						buf = ifOnlyIf(st[len(st)-2], st[len(st)-1])
+						buf, b = ifOnlyIf(st[len(st)-2], st[len(st)-1], res)
+						if buf && res {
+							if b {
+								valueByte[mass[i-1]] = 1
+							} else {
+								valueByte[mass[i-1]] = -1
+							}
+						}
 					}
 					if val == '=' {
-						buf = implies(st[len(st)-2], st[len(st)-1])
+						buf, b = implies(st[len(st)-2], st[len(st)-1], res)
+						if buf && res {
+							if b {
+								valueByte[mass[i-1]] = 1
+							} else {
+								valueByte[mass[i-1]] = -1
+							}
+						}
 					}
 					if val == '+' {
+
 						buf = and(st[len(st)-2], st[len(st)-1])
 					}
 					if val == '^' {
@@ -125,8 +179,7 @@ func nores(value byte, res []byte) bool {
 func binapp(add map[byte]bool, history []byte, addbin int) map[byte]bool {
 
 	buf := addbin
-	log.Printf("%b \n", addbin)
-	log.Println("bin ", len(history), string(history))
+
 	i := len(history) - 1
 	for i > -1 {
 
@@ -146,85 +199,100 @@ func binapp(add map[byte]bool, history []byte, addbin int) map[byte]bool {
 	return add
 }
 
-func cancel(val map[byte]bool, history []byte) bool{
+func cancel(val map[byte]bool, history []byte) bool {
 
-	for _, v := range history{
-		if val[v] == false{
+	for _, v := range history {
+		if val[v] == false {
 			return false
 		}
 	}
 	return true
 }
 
-func computation(mass [][]byte, val map[byte]bool, res []byte) {
+func computation(mass [][]byte, val map[byte]int, res []byte) {
 
-	var valueByte map[byte]bool
-	//result := make([]byte, 0)
-
-	var history []byte
-	valueByte = make(map[byte]bool)
-
-	for k, v := range val {
-		valueByte[k] = v
-		if v == false && nores(k, res) {
-			history = append(history, k)
-		}
-	}
-	for _ ,v := range res{
-		if valueByte[v] == false{
-		history = append(history, v)
-		}
-	}
-	
 	flag := 0
-	addbin := 0
-	log.Println(string(history))
-	minbaf := make(map[byte]bool)
-	min := -1
 	for {
-		for {
-			flag = 0
-			valueByte = binapp(valueByte, history, addbin)
-			for i := range mass {
-				if recurs(mass[i], valueByte)  == false{
-					log.Println(recurs(mass[i], valueByte),string(mass[i]) ,"lox")
-					flag = 1
-				}
-			}
-			if flag == 0 || cancel(valueByte, history) {
-				log.Printf("Done")
-				break
-			}
-
-			addbin++
-		}
-		buf := 0
-		for _, v := range valueByte {
-			if v == true {
-				buf++
+		for i := range mass {
+			if !recurs(mass[i], val) {
+				flag = 1
 			}
 		}
-		if (buf < min || min == -1) && flag == 0 {
-			min = buf
-			for k, v := range valueByte {
-				minbaf[k] = v
-			}
+		for key, v := range val {
+			log.Println(string(key), v)
 		}
-		if cancel(valueByte, history) { //алгоритм окончания другой (когда все тру)
+		if flag == 0 {
 			break
 		}
-		addbin++
+		flag = 0
 	}
-
+	//var valueByte map[byte]bool
+	////result := make([]byte, 0)
+	//
+	//var history []byte
+	//valueByte = make(map[byte]bool)
+	//
+	//for k, v := range val {
+	//	valueByte[k] = v
+	//	if v == false && nores(k, res) {
+	//		history = append(history, k)
+	//	}
+	//}
+	//for _ ,v := range res{
+	//	if valueByte[v] == false{
+	//	history = append(history, v)
+	//	}
+	//}
+	//
+	//flag := 0
+	//addbin := 0
+	//log.Println(string(history))
+	//minbaf := make(map[byte]bool)
+	//min := -1
+	//for {
+	//	for {
+	//		flag = 0
+	//		valueByte = binapp(valueByte, history, addbin)
+	//		for i := range mass {
+	//			if recurs(mass[i], valueByte)  == false{
+	//				log.Println(recurs(mass[i], valueByte),string(mass[i]) ,"lox")
+	//				flag = 1
+	//			}
+	//		}
+	//		if flag == 0 || cancel(valueByte, history) {
+	//			log.Printf("Done")
+	//			break
+	//		}
+	//
+	//		addbin++
+	//	}
+	//	buf := 0
+	//	for _, v := range valueByte {
+	//		if v == true {
+	//			buf++
+	//		}
+	//	}
+	//	if (buf < min || min == -1) && flag == 0 {
+	//		min = buf
+	//		for k, v := range valueByte {
+	//			minbaf[k] = v
+	//		}
+	//	}
+	//	if cancel(valueByte, history) { //алгоритм окончания другой (когда все тру)
+	//		break
+	//	}
+	//	addbin++
+	//}
+	//
 	fmt.Printf("RESULT: ")
-	for key, value := range minbaf {
+	for key, value := range val {
 
 		fmt.Printf("key %s val %t \n", string(key), value)
 	}
 	//fmt.Printf("res %s \n",string(res))
 	for _, va := range res {
-		if _, ok := minbaf[va]; ok {
-			fmt.Printf("RESULT %s %+v \n", string(va), minbaf[va])
+		if _, ok := val[va]; ok {
+			fmt.Printf("RESULT %s %+v \n", string(va), val[va])
 		}
 	}
 }
@@ -279,12 +347,12 @@ func polsky(valOper [][]byte, oper map[byte]int) [][]byte {
 
 }
 
-func sortCalc(mass [][]byte, val map[byte]bool) [][]byte {
+func sortCalc(mass [][]byte, val map[byte]int) [][]byte {
 
 	newSlice := make([][]byte, 0)
 	i := 0
 	for i < len(mass) {
-//		fmt.Printf("mass i %s %d\n", mass[i], i)
+		//		fmt.Printf("mass i %s %d\n", mass[i], i)
 		for _, value := range mass[i] {
 			if _, ok := val[value]; ok && len(mass) > 2 {
 				newSlice = append(newSlice, mass[i])
@@ -307,11 +375,11 @@ func sortCalc(mass [][]byte, val map[byte]bool) [][]byte {
 	return newSlice
 }
 
-func getOpsLines(lines []string) []string{
+func getOpsLines(lines []string) []string {
 	i := 0
 	res := make([]string, 0)
-	for i < len(lines){
-		if lines[i][0] != '?' && lines[i][0] != '='{
+	for i < len(lines) {
+		if lines[i][0] != '?' && lines[i][0] != '=' {
 			res = append(res, lines[i])
 		}
 		i++
@@ -319,22 +387,28 @@ func getOpsLines(lines []string) []string{
 	return res
 }
 
-func calculv2(lines []string, val map[byte]bool, res []byte){
+func calculv2(lines []string, val map[byte]int, res []byte) {
 	mass := make([][]byte, 0)
-//	buf := make([]byte, 0)
+	//	buf := make([]byte, 0)
 	//j := 0
 	i := 0
 	oper := initOper() // TODO: it's a global var
 	ops := getOpsLines(lines)
-	for i < len(ops){
+	for i < len(ops) {
 		k := 0
 		buf := make([]byte, 0)
 		for k < len(ops[i]) {
 			if ops[i][k] == '<' && k < len(ops[i])-2 && ops[i][k]+1 == '=' && ops[i][k+2] == '>' {
 				buf = append(buf, '-')
 				k += 2
+
 			} else {
-				buf = append(buf, ops[i][k])
+				if ops[i][k] == '=' && k < len(ops[i])-2 && ops[i][k]+1 == '>' {
+					buf = append(buf, '=')
+					k += 1
+				} else {
+					buf = append(buf, ops[i][k])
+				}
 			}
 			k++
 		}
@@ -350,8 +424,7 @@ func calculv2(lines []string, val map[byte]bool, res []byte){
 	computation(mass, val, res)
 }
 
-
-func calcul(data []byte, val map[byte]bool, res []byte) {
+func calcul(data []byte, val map[byte]int, res []byte) {
 	mass := make([][]byte, 0)
 	buf := make([]byte, 0)
 	j := 0
@@ -387,43 +460,44 @@ func calcul(data []byte, val map[byte]bool, res []byte) {
 	computation(mass, val, res)
 }
 
-func parseData(lines []string, val map[byte]bool, res []byte) {
+func parseData(lines []string, val map[byte]int, res []byte) {
 	calculv2(lines, val, res)
 }
 
-func removeComment(line string) string{
+func removeComment(line string) string {
 	pepega := strings.Split(line, "#")
 	return pepega[0]
 }
 
-func isComment(line string) bool{
+func isComment(line string) bool {
 	i := 0
-	for i < len(line){
-		if line[i] == ' '{
+	for i < len(line) {
+		if line[i] == ' ' {
 			i++
-		} else{
+		} else {
 			break
 		}
 	}
 	return line[i] == '#'
 }
 
-func isEmpty(line string) bool{
-	i:= 0
-	for i < len(line){
-		if line[i] != ' '{
-			return false}
+func isEmpty(line string) bool {
+	i := 0
+	for i < len(line) {
+		if line[i] != ' ' {
+			return false
+		}
 		i++
 	}
 	return true
 }
 
-func removeInvalid(line string) string{
+func removeInvalid(line string) string {
 	i := 0
-	for i < len(line){
-		if strings.Contains(allowedSymbols, string(line[i])){
+	for i < len(line) {
+		if strings.Contains(allowedSymbols, string(line[i])) {
 			i++
-		}else{
+		} else {
 			line = strings.ReplaceAll(line, string(line[i]), "")
 			i = 0
 		}
@@ -436,33 +510,33 @@ func parserv2(file *os.File) ([]string, error) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
-		if err := scanner.Err(); err != nil{
+		if err := scanner.Err(); err != nil {
 			return nil, err
 		}
 		lineLen := len(text)
-		if lineLen < 1 || isEmpty(text) || isComment(text){
+		if lineLen < 1 || isEmpty(text) || isComment(text) {
 			continue
 		}
-		if strings.Contains(text, "#"){
+		if strings.Contains(text, "#") {
 			text = removeComment(text)
 		}
 		text = removeInvalid(text)
-		if len(text) > 0{
+		if len(text) > 0 {
 			result = append(result, text)
 		}
 	}
 	return result, nil
 }
 
-func findFacts(lines []string) map[byte]bool{
-	val := make(map[byte]bool)
+func findFacts(lines []string) map[byte]int {
+	val := make(map[byte]int)
 	i := 0
-	for i < len(lines){
+	for i < len(lines) {
 		k := 0
-		if lines[i][k] != '?' && lines[i][k] != '='{
+		if lines[i][k] != '?' && lines[i][k] != '=' {
 			for k < len(lines[i]) {
 				if lines[i][k] >= 'A' && lines[i][k] <= 'Z' {
-					val[lines[i][k]] = false
+					val[lines[i][k]] = 0
 				}
 				k++
 			}
@@ -472,14 +546,14 @@ func findFacts(lines []string) map[byte]bool{
 	return val
 }
 
-func updateFacts(lines []string, val map[byte]bool){
+func updateFacts(lines []string, val map[byte]int) {
 	i := 0
-	for i < len(lines){
+	for i < len(lines) {
 		if lines[i][0] == '=' && len(lines[i]) > 1 {
 			k := 0
-			for k < len(lines[i]){
-				if lines[i][k] >= 'A' && lines[i][k] <= 'Z'{
-					val[lines[i][k]] = true
+			for k < len(lines[i]) {
+				if lines[i][k] >= 'A' && lines[i][k] <= 'Z' {
+					val[lines[i][k]] = 1
 				}
 				k++
 			}
@@ -488,15 +562,16 @@ func updateFacts(lines []string, val map[byte]bool){
 	}
 }
 
-func getUnknown(lines []string) []byte {
+func getUnknown(lines []string, val map[byte]int) []byte {
 	res := make([]byte, 0)
 	i := 0
-	for i < len(lines){
+	for i < len(lines) {
 		k := 0
-		if lines[i][k] == '?'{
-			for k < len(lines[i]){
-				if lines[i][k] >= 'A' && lines[i][k] <= 'Z'{
+		if lines[i][k] == '?' {
+			for k < len(lines[i]) {
+				if lines[i][k] >= 'A' && lines[i][k] <= 'Z' {
 					res = append(res, lines[i][k])
+					val[lines[i][k]] = 0
 				}
 				k++
 			}
@@ -506,16 +581,16 @@ func getUnknown(lines []string) []byte {
 	return res
 }
 
-func parseDatav2(lines []string) (map[byte]bool, []byte){
+func parseDatav2(lines []string) (map[byte]int, []byte) {
 
 	val := findFacts(lines)
-	if len(val) < 1{
+	if len(val) < 1 {
 		fmt.Printf("Failed to find Facts")
 		os.Exit(0)
 	}
 	updateFacts(lines, val)
-	res := getUnknown(lines)
-	if len(res) < 1{
+	res := getUnknown(lines, val)
+	if len(res) < 1 {
 		fmt.Printf("Failed to find Unknown Values")
 		os.Exit(0)
 	}
@@ -525,22 +600,20 @@ func parseDatav2(lines []string) (map[byte]bool, []byte){
 func validateData(lines []string) int {
 	i := 0
 	res := 0
-	if len(lines) < 3{
+	if len(lines) < 3 {
 		return 0
 	}
-	for i < len(lines){
-		if(lines[i][0] == '='){
+	for i < len(lines) {
+		if lines[i][0] == '=' {
 			res++
 		}
-		if (lines[i][0] == '?') {
+		if lines[i][0] == '?' {
 			res++
 		}
 		i++
 	}
 	return res
 }
-
-
 
 func main() {
 
@@ -559,7 +632,7 @@ func main() {
 			fmt.Println(err.Error())
 			return
 		} else {
-			if validateData(data) != 2{
+			if validateData(data) != 2 {
 				fmt.Printf("ivalid input")
 				return
 			}
